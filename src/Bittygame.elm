0a700c6390b708f6fd4b0aad7898f8ad3c59d135
Jessica Kerr <jessitron@outpace.com>
2015-09-12 18:52:03 +0000

    see the mouse signal

--- src/Bittygame.elm
+++ src/Bittygame.elm
@@ -1,7 +1,14 @@
 module Bittygame where
 
 import Html exposing (Html)
+import Signal
+import Mouse
 
-main : Html
-main = Html.text "yo"
+main : Signal Html
+main = Signal.map pointerView whereIsThePointer
 
+whereIsThePointer: Signal (Int, Int)
+whereIsThePointer = Mouse.position
+
+pointerView: (Int, Int) -> Html
+pointerView (x, y) = Html.text ("you are pointing at " ++ (toString x) ++ ", " ++ (toString y))
